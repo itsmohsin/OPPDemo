22887ef5e5154f3d066e3ae808fbbadbe9e7cc04
itsmohsin <mohsinsmannat@gmail.com>
2022-08-23 18:33:02 +0530

    Completed Day 2

--- app/src/main/java/com/itsmohsin/oppdemo/MainActivity.kt
+++ app/src/main/java/com/itsmohsin/oppdemo/MainActivity.kt
@@ -8,11 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val d = Driver("Tom", 37)
-//        d.showDetails()
-
         val myCar = MyCar()
-        myCar.maxSpeed = 890
+        myCar.maxSpeed = 300
         myCar.start()
     }
 }
